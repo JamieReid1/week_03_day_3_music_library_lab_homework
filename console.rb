9e6c4b5b3d771404cbec44d8e0b15cf00cb8bb84
Jamie Reid <wjreid85.jr@gmail.com>
2018-12-05 15:19:44 +0000

    Create albums and save albums method.

--- console.rb
+++ console.rb
@@ -10,6 +10,11 @@ artist1.save
 artist2 = Artist.new({ 'name' => 'Cher' })
 artist2.save
 
+album1 = Album.new({ 'title' => 'The Essential Elvis Presley', 'genre' => 'Rock and Roll', 'artist_id' => artist1.id })
+album1.save
+
+album2 = Album.new({ 'title' => 'Believe', 'genre' => 'Pop', 'artist_id' => artist2.id })
+album2.save
 
 
 
--- models/album.rb
+++ models/album.rb
@@ -1,6 +1,7 @@
 
 require('pg')
 require_relative('artist')
+require_relative('../db/sql_runner')
 
 class Album
 
@@ -12,6 +13,19 @@ class Album
     @title = options['title']
     @genre = options['title']
     @artist_id = options['artist_id'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO albums (
+             title,
+             genre,
+             artist_id
+            ) VALUES (
+               $1, $2, $3
+              )
+          RETURNING id"
+    values = [@title, @genre, @artist_id]
+    @id = SqlRunner.run(sql, values)
   end
 
 
